546ba9550a46b39ce9b43ad2895a297bf466fcc0
Marc <marc.bluemner@gmail.com>
2014-11-04 16:54:31 +0100

    Added a page to list the posts.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,24 @@
+function equalizeHeights(items) {
+  var maxHeight = 0;
+  items.each(function() {
+    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+  });
+  items.each(function() { $(this).css('min-height', (maxHeight + 10) + 'px'); });
+}
+
+function formatPostListings() {
+  var posts = $('body.page-posts div.posts div.thumbnail');
+  posts.each(function(){ $(this).css('min-height', "0px"); });
+  if( $(window).width() >= 992 ) {
+    for(var i = 0; i < posts.length; i+=3) { equalizeHeights( posts.slice(i, i+3) ); }
+  } else if ( $(window).width() >= 768 ) {
+    for(var i = 0; i < posts.length; i+=2) { equalizeHeights( posts.slice(i, i+2) ); }
+  }
+}
 $(document).ready(function(){
+
+  $(window).on('load', formatPostListings);
+$(window).on('resize', formatPostListings);
 
   // if they click on a boxed image, enlarge it
   $('div.img-box a').click(function(event) {
